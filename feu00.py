d07f1b65054de5953130ab4d06b460ed71bf96e6
MickaelMasson <masson.mickael@me.com>
2025-01-20 11:26:00 +0100

    Echauffement

--- feu00.py
+++ feu00.py
@@ -15,66 +15,88 @@ o
 
 Gérer les problèmes potentiels d’arguments."""
 
-import sys
 
-# Fonctions utilisées
+######################   Partie 1 :  Fonctions utilisées   ######################
+
+
+
+
+import sys
 def display_rectangle(rectangle_width: int, rectangle_height: int) :
-    angle_character = "o"
+
+    corner_character = "o"
     horizontal_character = "-"
     vertical_character = "|"
-    space_character = " "
-    first_and_last_line = angle_character + horizontal_character * (rectangle_width - 2) + (angle_character if rectangle_width > 1 else "")
-    intermediate_line = vertical_character + space_character * (rectangle_width - 2) + (vertical_character if rectangle_width > 1 else "")
-    for i in range(1, rectangle_height + 1) :
-        if i == 1 or i == rectangle_height :
-            print(first_and_last_line)
-        else :
+    empty_character = " "
+
+    first_or_last_line = corner_character + horizontal_character * \
+        (rectangle_width - 2) + (corner_character if rectangle_width > 1 else "")
+
+    intermediate_line = vertical_character + empty_character * \
+        (rectangle_width - 2) + (vertical_character if rectangle_width > 1 else "")
+
+    for i in range(rectangle_height):
+        if i == 0 or i == rectangle_height - 1:
+            print(first_or_last_line)
+        else:
             print(intermediate_line)
     return
 
-# Partie 1 : Gestion d'erreur
-def is_valid_arguments(arguments: list[str], number_of_argument: int) -> bool:
-    if len(arguments) != number_of_argument :
-        print("Error, le nombre d'arguments n'est pas valide")
+
+#######################   Partie 2 :  Gestion d'erreur   ########################
+
+def is_valid_arguments(is_number_of_argument_expected: bool) -> bool:
+    if not is_number_of_argument_expected:
+        print("Error : le nombre d'arguments n'est pas valide")
         return False
     return True
-    
+
+
 def is_digit(string: str) -> bool:
-    for character in string :
-        if not "0" <= character <= "9" :
+    for character in string:
+        if not "0" <= character <= "9":
             print(f"Error, '{string}' n'est pas un nombre entier positif")
             return False
     return True
 
-def is_greater_than(argument: int, index: int) -> bool:
-    if argument < index :
-        print(f"Error, la taille minimum de votre rectangle doit supérieur ou égale à {index}")
+
+def is_greater_than(is_min_rectangle_size: bool) -> bool:
+    if not is_min_rectangle_size:
+        print(f"Error, la taille minimum de votre rectangle doit etre de 1x1")
         return False
     return True
 
-# Partie 2 : Parsing
-def get_arguments() -> list[str] :
+
+############################   Partie 3 :  Parsing   ############################
+
+def get_arguments() -> list[str]:
     arguments = sys.argv[1:]
     return arguments
 
-# Partie 3 : Résolution
-def get_rectangle() :
+
+##########################   Partie 4 :  Résolution   ###########################
+
+def get_rectangle() -> None:
     arguments = get_arguments()
-    number_of_argument_expected = 2
-    if not is_valid_arguments(arguments, number_of_argument_expected) :
-        return
-    for argument in arguments :
-        if not is_digit(argument) :
+    if not is_valid_arguments(len(arguments) == 2):
+        return None
+
+    for argument in arguments:
+        if not is_digit(argument):
             return
     numbers = list(map(int, arguments))
-    min_rectangle_size = 1
-    for number in numbers :
-        if not is_greater_than(number, min_rectangle_size) :
+
+    for number in numbers:
+        if not is_greater_than(number >= 1):
             return
+
     rectangle_width = numbers[0]
     rectangle_height = numbers[1]
 
     display_rectangle(rectangle_width, rectangle_height)
+    return
 
-# Partie 4 : Affichage
+
+###########################   Partie 5 :  Affichage   ###########################
+
 get_rectangle()
